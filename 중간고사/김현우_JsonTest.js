e99def74bd5431a2c068e8f6bb598dbe10bd90bb
askl1128 <josan1128@gmail.com>
2021-11-18 13:49:11 +0900

    .

---
+++ 중간고사/김현우_JsonTest.js
@@ -0,0 +1,7 @@
+const data = { 
+        "code":42,
+        "item":[{"id":"name"},{"1":"foo"},{"2":"bar"}]
+        
+}; 
+var n = 1; 
+console.log('아이템 아이디는 ' + data.item + '이고, 아이템 이름은 ' + data.item + '입니다')
